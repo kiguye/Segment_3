342cee5c69005b2e19853bca496798f5e7aadc0c
Kiguye Collins <kiguyecollins@gmail.com>
2023-04-22 14:53:38 +0900

    first commit

---
+++ 01_Capturing_Events_WIth_JS.js
@@ -0,0 +1,6 @@
+/**  Capturing Events with JavaScript.
+ *  
+ * (1) Events happen in JS.
+ * (2) JS can capture and handle events before they happen,
+ *     when happening and after the events happen.
+*/
---
+++ 01_JavaScript_name.js
@@ -0,0 +1,3 @@
+let normalName = "Collins";
+let javaScriptName = normalName + "Script";
+console.log("Your JavaScript name is " + javaScriptName);
